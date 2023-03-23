a35c66217b0b0b6022a189920c7a79211844c2bd
Doudou-BH <clement.oge59@gmail.com>
2023-03-23 17:20:39 +0100

    Improved findSurroundings

--- code/main.c
+++ code/main.c
@@ -1,6 +1,6 @@
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Generator configuration
 #define HEIGHT 30
@@ -15,22 +15,11 @@
 /*
  * TODO:
  * - Decide of the rules
- * - Ponderage de la distance
- */
-
-/*
- * idk if i'll need this again, so i kept it jsut in case
- * for (int i = 0; i < HEIGHT; i++)
-	{
-		for (int j = 0; j < LENGTH; j++)
-			printf("%d, ", res[i * LENGTH + j]);
-		printf("\n");
-	}
+ * - Pondérage de la distance
  */
 
 // The name is self-explanatory imo
-void clearStdout()
-{
+void clearStdout() {
 #ifdef _WIN32
   system("cls");
 #elif _WIN64
@@ -40,49 +29,51 @@ void clearStdout()
 #endif
 }
 
-void wait(int ms)
-{
-	struct timespec ts;
-	ts.tv_sec = ms / 1000;
-	ts.tv_nsec = (ms % 1000) * 1000000;
-	nanosleep(&ts, NULL);
+void wait(int ms) {
+  struct timespec ts;
+  ts.tv_sec = ms / 1000;
+  ts.tv_nsec = (ms % 1000) * 1000000;
+  nanosleep(&ts, NULL);
 }
 
 void printArray(int *arr, int n) {
-  for (int i = 0; i < n-1; i++)
+  for (int i = 0; i < n - 1; i++)
     printf("%d, ", arr[i]);
-  printf("%d\n", arr[n-1]);
+  printf("%d\n", arr[n - 1]);
 }
 
-void printGrid(int *grid)
-{
-  char *types[4] = {"  ", "\e[0;32m██\e[0m", "\e[0;31m██\e[0m", "\e[0;34m██\e[0m"};
+void printGrid(int *grid){
+  char *colors[4] = {
+    "  ",
+    "\e[0;32m██\e[0m",
+    "\e[0;31m██\e[0m",
+    "\e[0;34m██\e[0m"
+  };
 
-	printf("┏━");
-	for (int i = 0; i < LENGTH; i++)
-		printf("━━");
-	printf("━┓\n");
+  printf("┏━");
+  for (int i = 0; i < LENGTH; i++)
+    printf("━━");
+  printf("━┓\n");
 
-	for (int i = 0; i < HEIGHT; i++)
-	{
-		printf("┃ ");
-		for (int j = 0; j < LENGTH; j++)
-			printf("%s", types[grid[i * LENGTH + j]]);
-		printf(" ┃\n");
-	}
+  for (int i = 0; i < HEIGHT; i++) {
+    printf("┃ ");
+    for (int j = 0; j < LENGTH; j++)
+      printf("%s", colors[grid[i * LENGTH + j]]);
+    printf(" ┃\n");
+  }
 
-	printf("┗━");
-	for (int i = 0; i < LENGTH; i++)
-		printf("━━");
-	printf("━┛\n");
+  printf("┗━");
+  for (int i = 0; i < LENGTH; i++)
+    printf("━━");
+  printf("━┛\n");
 }
 
-void editCell(int *grid, int i, int value)
-{
+void editCell(int *grid, int i, int value) {
   if (i >= 0 && i < CELLS_COUNT)
     grid[i] = value;
 }
 
+/*
 int distance(int a, int b) {
   return abs((b % LENGTH) - (a % LENGTH)) + abs((b / LENGTH) - (a / LENGTH));
 }
@@ -98,15 +89,34 @@ int *findSurroundings(int *grid, int k) {
 
   return res;
 }
+*/
 
-void nextStep(int *grid)
-{
+int *findSurroundings(int *grid, int k) {
+  int *res = malloc(sizeof(int) * CELL_TYPE_COUNT);
+  for (int i = CELL_TYPE_COUNT; i--; res[i] = 0);
+
+  int kx = k % LENGTH,
+      ky = k / LENGTH;
+
+  for (int i = max(0, kx - NEIGHBOURS_DISTANCE); i <= min(LENGTH, kx + NEIGHBOURS_DISTANCE); i++)
+  {
+    for (int j = max(0, ky - NEIGHBOURS_DISTANCE); j <= min(HEIGHT, ky + NEIGHBOURS_DISTANCE); j++)
+    {
+      if (i == kx && j == ky) continue;
+      res[grid[i * LENGTH + j]]; // possibly i + j * LENGTH
+    }
+  }
+
+  return res;
+}
+
+
+void nextStep(int *grid) {
   int *res = malloc(sizeof(int) * CELLS_COUNT);
   for (int i = 0; i < CELLS_COUNT; i++)
     res[i] = grid[i];
-  
-  for (int i = 0; i < CELLS_COUNT; i++)
-  {
+
+  for (int i = 0; i < CELLS_COUNT; i++) {
     int *surroundings = findSurroundings(grid, i);
 
     // A house surrounded by houses becomes a store
@@ -118,58 +128,56 @@ void nextStep(int *grid)
       editCell(res, i, 0);
 
     // Some people like to settle in rural areas
-    else if (grid[i] == 0 && surroundings[1] <= 2 && rand() % 100 < (15 * (surroundings[2])))
+    else if (grid[i] == 0 && surroundings[1] <= 2 &&
+             rand() % 100 < (15 * (surroundings[2])))
       editCell(res, i, 1);
 
-    else if (grid[i] == 2 && (surroundings[1] <= 3 || surroundings[2] >= 4) && rand() % 100 < 25)
+    else if (grid[i] == 2 && (surroundings[1] <= 3 || surroundings[2] >= 4) &&
+             rand() % 100 < 25)
       editCell(res, i, rand() % 100 < 25);
-    
+
     /*
     if (grid[i] == 1)
     {
       // Change all cells around a house to a house
       if (i % LENGTH != 0 && rand() % 2)
-	editCell(res, i - 1, 1);
-      
+    editCell(res, i - 1, 1);
+
       if (i % LENGTH != LENGTH - 1 && rand() % 2)
-	editCell(res, i + 1, 1);
-      
+    editCell(res, i + 1, 1);
+
       if (rand() % 2)
-	editCell(res, i - LENGTH, 1);
-      
+    editCell(res, i - LENGTH, 1);
+
       if (rand() % 2)
-	editCell(res, i + LENGTH, 1);
+    editCell(res, i + LENGTH, 1);
     }
     */
   }
-  
-  // grid = res; // why not
-  
+
   for (int i = 0; i < CELLS_COUNT; i++)
     grid[i] = res[i];
-  
+
   free(res);
 }
 
-int main()
-{
+int main() {
   srand(time(NULL));
- 
+
   int *grid = malloc(sizeof(int) * CELLS_COUNT);
   for (int i = 0; i < CELLS_COUNT; i++)
     grid[i] = 0;
-  
+
   grid[rand() % (CELLS_COUNT)] = 2;
-  
-  for (;;)
-  {
+
+  for (;;) {
     fflush(stdout);
     clearStdout();
-    
+
     printGrid(grid);
     wait(PRINT_INTERVAL);
     nextStep(grid);
   }
-  
+
   return 0;
 }
